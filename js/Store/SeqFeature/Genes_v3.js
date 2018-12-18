d1354ff3993e0165188b059ba8e71657f931517a
Colin <colin.diesh@gmail.com>
2018-12-18 14:07:55 -0500

    Make the queries to mygene.info use nicer chunking strategy

--- js/Store/SeqFeature/Genes_v3.js
+++ js/Store/SeqFeature/Genes_v3.js
@@ -4,7 +4,8 @@ define([
     'dojo/_base/lang',
     'dojo/request',
     'JBrowse/Store/SeqFeature',
-    'JBrowse/Model/SimpleFeature'
+    'JBrowse/Model/SimpleFeature',
+    'JBrowse/Store/LRUCache'
 ],
 function(
     declare,
@@ -12,15 +13,60 @@ function(
     lang,
     request,
     SeqFeatureStore,
-    SimpleFeature
+    SimpleFeature,
+    LRUCache
 ) {
     return declare(SeqFeatureStore, {
         getFeatures: function(query, featureCallback, finishCallback, errorCallback) {
+            var cache = this.featureCache = this.featureCache || new LRUCache({
+                name: 'mygeneFeatureCache',
+                fillCallback: dojo.hitch(this, '_readChunk'),
+                sizeFunction: function (features) {
+                    return features.length;
+                },
+                maxSize: 100000
+            });
+            query.toString = () => `${query.ref},${query.start},${query.end}`
+			const chunkSize = 100000
+            const s = query.start - query.start % chunkSize
+            const e = query.end + (chunkSize - query.end % chunkSize)
+            const chunks = []
+            let chunksProcessed = 0
+            let haveError = false
+            for(let start = s; start < e; start += chunkSize) {
+                var chunk = { ref: query.ref, start: start, end: start + chunkSize }
+                chunk.toString = () => `${query.ref},${query.start},${query.end}`
+                chunks.push(chunk)
+            }
+            chunks.forEach(c => {
+                cache.get(c, (f, err) => {
+                    if(err && !haveError) {
+                        errorCallback(err)
+                    }
+                    haveError = haveError || err
+                    if(haveError) {
+                        return
+                    }
+                    console.log(f)
+                    f.forEach(feature => {
+                        if (feature.get('start') > query.end) {
+                            return
+                        } else if(feature.get('end') >= query.start) {
+                            featureCallback(feature)
+                        }
+                    })
+                    if(++chunksProcessed == chunks.length) {
+                        finishCallback()
+                    }
+                })
+            })
+        },
+        _readChunk(query, callback) {
             var url = this.resolveUrl(this.config.urlTemplate, { refseq: query.ref, start: query.start, end: query.end });
             var hg19 = +(this.config.hg19 || 0);
-
-            request(url, { handleAs: 'json' }).then(function(featuredata) {
-                array.forEach(featuredata.hits, function(f) {
+            var feats = [];
+            request(url, { handleAs: 'json' }).then(featuredata => {
+                featuredata.hits.forEach(f => {
                     var genomic_pos = [f.genomic_pos, f.genomic_pos_hg19][hg19];
                     var exons = [f.exons, f.exons_hg19][hg19];
                     if (lang.isArray(genomic_pos)) {
@@ -33,18 +79,18 @@ function(
                         delete f.exac;
                     }
                     if(f.reagent) {
-                        Object.keys(f.reagent).forEach(function(key) {
+                        Object.keys(f.reagent).forEach(key => {
                             f['reagent_'+key] = f.reagent[key];
                         });
                         delete f.reagent;
                     }
                     if(f.reporter) {
-                        Object.keys(f.reporter).forEach(function(key) {
+                        Object.keys(f.reporter).forEach(key => {
                             f['reporter_'+key] = f.reporter[key];
                         });
                         delete f.reporter;
                     }
-                        
+
                     var superfeat = new SimpleFeature({
                         id: f._id,
                         data: lang.mixin(lang.clone(f), {
@@ -75,42 +121,46 @@ function(
                         superfeat.data.subfeatures.push(feature);
                     }
                     var transcripts = {};
-                    array.forEach(exons, function(exon) {
-                        var tname = exon.transcript;
-                        var ts = new SimpleFeature({
-                            id: tname,
-                            data: lang.mixin({
-                                start: exon.txstart,
-                                end: exon.txend,
-                                strand: exon.strand,
-                                type: 'mRNA',
-                                name: tname,
-                                subfeatures: []
-                            }),
-                            parent: superfeat
-                        });
-                        array.forEach(exon.position, function(pos) {
-                            var subfeat = new SimpleFeature({
-                                data: lang.mixin(lang.clone(exon), {
-                                    start: pos[0],
-                                    end: pos[1],
-                                    strand: genomic_pos.strand,
-                                    position: null,
-                                    txstart: null,
-                                    txend: null,
-                                    transcript: null,
-                                    type: 'exon'
+                    if(exons) {
+                        exons.forEach(exon => {
+                            var tname = exon.transcript;
+                            var ts = new SimpleFeature({
+                                id: tname,
+                                data: lang.mixin({
+                                    start: exon.txstart,
+                                    end: exon.txend,
+                                    strand: exon.strand,
+                                    type: 'mRNA',
+                                    name: tname,
+                                    subfeatures: []
                                 }),
-                                parent: ts
+                                parent: superfeat
                             });
-                            ts.data.subfeatures.push(subfeat);
+                            exon.position.forEach(pos => {
+                                var subfeat = new SimpleFeature({
+                                    data: lang.mixin(lang.clone(exon), {
+                                        start: pos[0],
+                                        end: pos[1],
+                                        strand: genomic_pos.strand,
+                                        position: null,
+                                        txstart: null,
+                                        txend: null,
+                                        transcript: null,
+                                        type: 'exon'
+                                    }),
+                                    parent: ts
+                                });
+                                ts.data.subfeatures.push(subfeat);
+                            });
+                            superfeat.data.subfeatures.push(ts);
                         });
-                        superfeat.data.subfeatures.push(ts);
-                    });
-                    featureCallback(superfeat);
+                    }
+                    feats.push(superfeat)
                 });
-                finishCallback();
-            }, errorCallback);
+                callback(feats)
+            }, err => {
+                callback(null, err)
+            });
         }
     });
 });
